81cc35bc406ed1b706560fc06cfaf2926030ba13
amarquezmazzeo <amarquezmazz@mail.usf.edu>
2025-08-23 02:15:30 -0400

    more tests for htmlnode

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -20,8 +20,27 @@ class TestHTMLNode(unittest.TestCase):
                             "id": "test"
                         })
         props_result = 'class="hello-div" id="test"'
-        print(node2)
+        # print(node2)
         self.assertEqual(props_result, node2.props_to_html())
+
+    def test_eq2(self):
+        node = HTMLNode(tag="p",
+                        value="hello peeps",
+                        children=None,
+                        props={
+                            "class": "hello-text",
+                            "id": "salutation"
+                        })
+        node2 = HTMLNode(tag="d",
+                        value=None,
+                        children=[node],
+                        props={
+                            "class": "hello-div",
+                            "id": "test"
+                        })
+        props_result = 'class="hello-text" id="salutation"'
+        # print(node2)
+        self.assertEqual(props_result, node.props_to_html())
 
 
 if __name__ == "__main__":
