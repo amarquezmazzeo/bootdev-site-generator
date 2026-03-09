1498bb40cdc7cccc56f06c8d1d6b3b33542de9f6
amarquezmazzeo <amarquezmazz@mail.usf.edu>
2026-03-09 00:07:48 -0400

    adding logic to recursively copy static content to public dir

--- src/main.py
+++ src/main.py
@@ -1,25 +1,32 @@
-from textnode import TextNode, TextType
-from htmlnode import HTMLNode, LeafNode, ParentNode
+import os
+import shutil
 
-# print("hello world")
-# tex = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-# print(tex)
+CURR_DIR = os.path.abspath(__file__)
+ROOT_DIR = os.path.dirname(os.path.dirname(CURR_DIR))
+SOURCE_DIR = os.path.join(ROOT_DIR, 'static')
+TARGET_DIR = os.path.join(ROOT_DIR, 'public')
 
-def text_node_to_html_node(text_node):
-    if text_node.text_type == TextType.TEXT:
-        return LeafNode(tag=None, value=text_node.text)
-    elif text_node.text_type == TextType.BOLD:
-        return LeafNode(tag="b", value=text_node.text)
-    elif text_node.text_type == TextType.ITALIC:
-        return LeafNode(tag="i", value=text_node.text)
-    elif text_node.text_type == TextType.CODE:
-        return LeafNode(tag="code", value=text_node.text)
-    elif text_node.text_type == TextType.LINK:
-        return LeafNode(tag="a", value=text_node.text,
-                        props={"href" : text_node.url})
-    elif text_node.text_type == TextType.IMAGE:
-        return LeafNode(tag="img", value="",
-                        props={"src" : text_node.url,
-                               "alt": text_node.text}) 
-    else:
-        raise ValueError("unsupported text type")
+def copy_dir(dir):
+    source = os.path.join(SOURCE_DIR, dir)
+    target = os.path.join(TARGET_DIR, dir)
+    dir_contents = os.listdir(source)
+    dirs = []
+    if not os.path.exists(target):
+        os.mkdir(target)
+    for obj in dir_contents:
+        is_file = os.path.isfile(os.path.join(source,obj))
+        print(f"{obj} is file? {is_file}")
+        if is_file:
+            print(f"attempting to copy file {obj}")
+            shutil.copy(os.path.join(source, obj), os.path.join(target, obj))
+        else:
+            print(f"attempting to copy non-file {obj}")
+            copy_dir(os.path.join(dir,obj))
+        
+def main():
+    if os.path.exists(TARGET_DIR):
+        shutil.rmtree(TARGET_DIR)
+    copy_dir('')
+
+if __name__ == "__main__":
+    main()
